5f41c59991fde2114a24ec49aefb8f4d86118a60
Eric Barb <eric.barb@gmail.com>
2023-08-21 21:38:46 -0400

    Removing old terraform code

--- terraform/providers.tf
+++
@@ -1,7 +0,0 @@
-# Configure the VMware vSphere Provider
-provider "vsphere" {
-  vsphere_server       = var.vsphere_vcenter
-  user                 = var.vsphere_user
-  password             = var.vsphere_password
-  allow_unverified_ssl = true
-}
